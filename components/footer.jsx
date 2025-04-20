b91be893b32c4594c4c1f2a33dd69c96d544ce63
ZayGana25 <isaiahl25@hotmail.com>
2025-04-20 01:38:28 -0700

    footer update

--- components/footer.jsx
+++ components/footer.jsx
@@ -54,7 +54,7 @@ export default function Footer() {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-            <p className="text-sm text-muted-foreground">&copy; {new Date().getFullYear()} TheZayHub. Eugene, OR</p>
+            <p className="text-sm text-muted-foreground">&copy; {new Date().getFullYear()} TheZayHub </p>
           </div>
           <div className="flex space-x-4 mb-4 md:mb-0">
             <Button
