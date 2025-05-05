593b40c88fcb6e7ba874b564b453671c8ea1bb02
ZayGana25 <isaiahl25@hotmail.com>
2025-05-05 00:50:16 -0700

    SEO fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,9 +2,8 @@
 module.exports = {
     siteUrl: 'https://thezayhub.com', // Replace with your custom domain
     generateRobotsTxt: true,
-    changefreq: 'weekly',
     priority: 0.7,
     sitemapSize: 5000,
     exclude: ['/secret', '/api/*'],
-    outDir: './public/SEO',
+    outDir: './SEO',
   };
